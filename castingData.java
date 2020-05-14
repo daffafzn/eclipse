65e244632b96f6b086a09db8ecafd5f855074af2
daffafzn <daffa.fauzan@gmail.com>
2020-05-14 08:23:40 +0700

    First Commit

---
+++ castingData.java
@@ -0,0 +1,14 @@
+public class castingData{
+	public static void main(String[] args){
+		byte a = 50;
+		int b = 5;
+		int c = 2;
+		
+		int d = (a*b/c);
+		int e = (a*b)/251;
+		
+		System.out.println ("a = "+a+" b = "+b+" c = "+c+"d = "+d);
+		System.out.println ("e = "+e);
+		}
+	}
+	
